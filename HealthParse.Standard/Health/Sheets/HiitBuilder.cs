e56456ed7c540132c3fa9efa3adcf0edd0399956
Jon Fuller <jcfuller@sep.com>
2018-02-11 19:27:51 -0500

    add hiit

---
+++ HealthParse.Standard/Health/Sheets/HiitBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using NodaTime;
+
+namespace HealthParse.Standard.Health.Sheets
+{
+    public class HiitBuilder : WorkoutBuilder
+    {
+        public HiitBuilder(IReadOnlyDictionary<string, IEnumerable<Workout>> workouts, DateTimeZone zone)
+            : base(workouts, HKConstants.Workouts.Hiit, zone, r => new
+            {
+                date = r.StartDate.InZone(zone),
+                duration = r.Duration,
+            })
+        {
+        }
+    }
+}
