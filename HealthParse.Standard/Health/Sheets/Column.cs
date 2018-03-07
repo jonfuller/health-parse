6ea3915dc30d21da0038581c532cbb0937489d1e
Jon Fuller <jcfuller@sep.com>
2018-03-07 13:10:30 -0500

    remove unused code

--- HealthParse.Standard/Health/Sheets/Column.cs
+++ HealthParse.Standard/Health/Sheets/Column.cs
@@ -23,7 +23,6 @@ namespace HealthParse.Standard.Health.Sheets
         public object this[TKey key] => _data.GetValue(key);
 
         public IEnumerable<object> Values => _data.Values;
-        public IEnumerable<TKey> Keys => _data.Keys;
 
         public IEnumerator<TKey> GetEnumerator()
         {
