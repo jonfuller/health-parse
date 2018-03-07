0e6f4f1b23d7872a01e50e49603e8719638ede7c
Jon Fuller <jcfuller@sep.com>
2018-03-07 12:42:22 -0500

    actually add sheetnames constant file

---
+++ HealthParse.Standard/Health/Sheets/SheetNames.cs
@@ -0,0 +1,20 @@
+﻿namespace HealthParse.Standard.Health.Sheets
+{
+    public static class SheetNames
+    {
+        public const string Summary = "Overall Summary";
+        public const string Steps = "Steps";
+        public const string Mass = "Mass (Weight)";
+        public const string BodyFat = "Body Fat %";
+        public const string GeneralRecords = "General Records";
+        public const string CyclingDistance = "Cycling (Distance)";
+        public const string CyclingWorkouts = "Cycling (Workouts)";
+        public const string StrengthTraining = "Strength Training";
+        public const string Hiit = "HIIT";
+        public const string Running = "Running";
+        public const string Walking = "Walking";
+        public const string Elliptical = "Elliptical";
+        public const string Play = "Play";
+        public const string Settings = "Settings";
+    }
+}
